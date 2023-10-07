f9def92380d40ee30800609db5f22c3cd511efb5
David Diggs <ddiggs34@gmail.com>
2023-10-07 12:00:32 -0400

    done

---
+++ Classes/Car.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Car
+    {
+
+        //Create a property Make, Model, Year - DONE
+        public string Make { get; set; }
+        public string Model {  get; set; }
+        public int Year { get; set; }
+
+        //constructor
+
+        public Car() 
+        { 
+        
+        
+        }
+
+    }
+
+    
+    
+
+       
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Classes
+﻿namespace Classes;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        //Instance of a class - DONE
+
+      Car dreamCar = new Car(); //made a new type and calling the constructor from custom class
+
+        dreamCar.Make = "Nissan";
+        dreamCar.Model = "GTR";
+        dreamCar.Year = 2020;
+
+        Console.WriteLine($"My dream car is a {dreamCar.Year} {dreamCar.Make} {dreamCar.Model}");
+
+
+
+
     }
 }
